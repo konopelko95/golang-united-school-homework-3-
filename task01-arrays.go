2517ff60bc289d0ad4812ea9d1d5dbc22721027e
Vlad Zenbook <vlad.konopelko.dku@gmail.com>
2022-05-15 15:57:19 +0600

    1 and 2 tasks

--- task01-arrays.go
+++ task01-arrays.go
@@ -2,5 +2,28 @@ package homework
 
 func average(input [15]float32) (result float32) {
 	//Place your code here
-	return
+
+	//объявление массива
+	var a [15]float32
+	a[0] = 1
+	a[1] = 2
+	a[2] = 3
+	a[3] = 4
+	a[4] = 5
+	a[5] = 6
+
+	// посчитать сумму массива и //поссчитать кол-во элементов не равных 0
+	var sum float32 = 0
+	var i float32 = 0
+	for _, value := range a {
+		if value != 0 {
+			i++
+		}
+		sum += value
+	}
+
+	//посчитать среднее
+	result = (sum / i)
+
+	return result
 }
--- task02-slice.go
+++ task02-slice.go
@@ -2,5 +2,19 @@ package homework
 
 func reverse(input []int64) (result []int64) {
 	//Place your code here
-	return
+	a := []int64{1, 2, 5, 15}
+
+	//создание копии
+
+	b := make([]int64, len(a))
+	c := make([]int64, len(a))
+
+	copy(b, a)
+
+	//реверз копии
+	for i := len(b) - 1; i >= 0; i-- {
+		c = append(c, a[i])
+	}
+
+	return c
 }
