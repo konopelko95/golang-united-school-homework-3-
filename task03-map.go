3d077d144319e7db4fb16bc13d2e017a28a9e231
Vlad Zenbook <vlad.konopelko.dku@gmail.com>
2022-05-16 14:19:00 +0600

    task 3 added

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,27 @@
 package homework
 
+import (
+	"sort"
+)
+
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
+	map1 := map[int]string{2: "a", 0: "b", 1: "c"}
+
+	keys := make([]int, 0, len(map1))
+	for k := range map1 {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+
+	map2 := map[int]string{10: "aa", 0: "bb", 500: "cc"}
+
+	keys2 := make([]int, 0, len(map2))
+	for k2 := range map2 {
+		keys2 = append(keys2, k2)
+	}
+
+	sort.Ints(keys2)
+
 	return
 }
